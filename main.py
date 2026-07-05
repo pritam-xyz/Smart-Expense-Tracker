84638014fe1045d89a652e983447f50b6aca532c
Athox-X <kumarpritam65686@gmail.com>
2026-07-05 20:16:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,16 +10,17 @@ if not os.path.exists(file_name):
     writer.writerow(["Title", "Amount"])
     file.close()
 
-
 while True:
     print("\n===== Expense Tracker =====")
     print("1. Add Expense")
     print("2. View Expenses")
     print("3. Total Expense")
-    print("4. Exit")
+    print("4. Delete Expense")
+    print("5. Exit")
 
     choice = input("Enter your choice: ")
 
+    # Add Expense
     if choice == "1":
         title = input("Expense Name: ")
         amount = input("Amount: ")
@@ -31,6 +32,7 @@ while True:
 
         print("Expense Added Successfully!")
 
+    # View Expenses
     elif choice == "2":
         file = open(file_name, "r")
         reader = csv.reader(file)
@@ -42,6 +44,7 @@ while True:
 
         file.close()
 
+    # Total Expense
     elif choice == "3":
         file = open(file_name, "r")
         reader = csv.DictReader(file)
@@ -55,7 +58,47 @@ while True:
 
         print("\nTotal Expense = ₹", total)
 
+    # Delete Expense
     elif choice == "4":
+        file = open(file_name, "r")
+        reader = csv.DictReader(file)
+
+        expenses = []
+
+        print("\n------ Expenses ------")
+        index = 1
+
+        for row in reader:
+            expenses.append(row)
+            print(index, ".", row["Title"], "- ₹", row["Amount"])
+            index += 1
+
+        file.close()
+
+        if len(expenses) == 0:
+            print("No Expenses Found!")
+        else:
+            delete = int(input("Enter expense number to delete: "))
+
+            if 1 <= delete <= len(expenses):
+                expenses.pop(delete - 1)
+
+                file = open(file_name, "w", newline="")
+                writer = csv.writer(file)
+
+                writer.writerow(["Title", "Amount"])
+
+                for expense in expenses:
+                    writer.writerow([expense["Title"], expense["Amount"]])
+
+                file.close()
+
+                print("Expense Deleted Successfully!")
+            else:
+                print("Invalid Number!")
+
+    # Exit
+    elif choice == "5":
         print("Thank You!")
         break
 
